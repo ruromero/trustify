04a8839c6fbc62990320ff56d594ef36b2480b6f
Jens Reimann <ctron@dentrassi.de>
2026-01-13 11:07:48 +0100

    test: disable until we have the first upload

--- migration/tests/previous.rs
+++ migration/tests/previous.rs
@@ -6,6 +6,7 @@ use trustify_test_context::TrustifyMigrationContext;
 /// test to see if we can import a dump from a previous commit and migrate, including the data
 #[test_context(TrustifyMigrationContext)]
 #[test(tokio::test)]
+#[ignore = "Enable after first branch upload was successful"]
 async fn from_previous(ctx: &TrustifyMigrationContext) -> Result<(), anyhow::Error> {
     // We automatically start with a database imported from the previous commit.
     // But we haven't migrated to the most recent schema so far. That's done by the next step.
