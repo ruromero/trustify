46598cb4fed2bf1d9a67f67ac554eaf4b8d746df
rh-jfuller <jfuller@redhat.com>
2026-09-22 18:02:41 +0200

    fix(migration): normalize version function parallel safety

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -73,6 +73,7 @@ mod m0002280_backfill_sbom_suppliers;
 mod m0002290_create_exploit_intelligence_job;
 mod m0002300_create_exploit;
 mod m0002360_pythonver_cmp_parallel_restricted;
+mod m0002380_version_function_parallelism;
 
 pub trait MigratorExt: Send {
     fn build_migrations() -> Migrations;
@@ -161,6 +162,7 @@ impl MigratorExt for Migrator {
             .normal(m0002290_create_exploit_intelligence_job::Migration)
             .normal(m0002300_create_exploit::Migration)
             .normal(m0002360_pythonver_cmp_parallel_restricted::Migration)
+            .normal(m0002380_version_function_parallelism::Migration)
     }
 }
 
